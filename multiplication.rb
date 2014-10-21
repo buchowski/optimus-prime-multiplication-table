077d2e9fcee0e8f2fa47f99a8b74302f13b44d8b
buchowski <bucholz.adam@gmail.com>
2014-10-20 20:41:32 -0700

    pass initial tests

--- multiplication.rb
+++ multiplication.rb
@@ -1,3 +1,12 @@
 def primeNumbers(n)
+    primes = []
+    i = 1
 
+    while primes.size < n do
+        prime = (2...i).all? { |j| i % j != 0 }
+        primes.push(i) if prime
+        i += 1
+    end
+
+    return primes
 end
